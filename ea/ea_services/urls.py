c3bf9819ed2d8b76bbbc8088bcd06b411442821a
Sam Fries <fries2@llnl.gov>
2016-08-12 11:55:48 -0700

    More import shenanigans

--- ea/ea_services/urls.py
+++ ea/ea_services/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^group/(?P<gid>\d+)/remove_user/$', views.remove_user_from_group, name="remove-user-from-group"),
     url(r'^group/(?P<gid>\d+)/add_user/$', views.add_user_to_group, name="add-user-to-group"),
     url(r'^group/share_dataset/$', views.share_dataset, name="share-dataset"),
-    url(r'^credentials/(?P<username>[A-Z0-9a-z\-]+)/$', retrieve_key),
+    url(r'^credentials/(?P<username>[A-Z0-9a-z\-]+)/$', views.retrieve_key),
     url(r'^upload/(?P<dataset_name>[^/]+)/$', views.upload_files)
 ]
