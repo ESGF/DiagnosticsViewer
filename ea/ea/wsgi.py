99bdc4926b8ad8fee74dc11b9ea5aea3585d97a8
Sam Fries <fries2@llnl.gov>
2016-08-12 11:53:49 -0700

    Remove hacky pythonpath stuff from wsgi module; should be handled in the deployment

--- ea/ea/wsgi.py
+++ ea/ea/wsgi.py
@@ -11,14 +11,6 @@ import os
 import sys
 import site
 
-app_root = os.path.dirname(os.path.dirname(__file__))
-
-# Requires environment to be installed above the app_root and named "env"
-site.addsitedir(os.path.join(os.path.dirname(app_root), "env", "lib", "python2.7", "site-packages"))
-
-if app_root not in sys.path:
-    sys.path.append(app_root)
-
 os.environ.setdefault("UVCDAT_ANONYMOUS_LOG", "false")
 os.environ.setdefault("HOME", "/var/www")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ea.settings")
